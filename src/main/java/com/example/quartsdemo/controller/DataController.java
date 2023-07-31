cae0e2000c54c3b7aae08f31036ab891550919d3
Jgu0822 <jgu96@naver.com>
2023-07-31 14:43:52 +0900

    UI 수정

--- src/main/java/com/example/quartsdemo/controller/DataController.java
+++ src/main/java/com/example/quartsdemo/controller/DataController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/data")
 @Slf4j
-public class DataCotroller {
+public class DataController {
 
     @Autowired
     private DataService dataService;
